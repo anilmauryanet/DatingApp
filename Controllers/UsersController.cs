4fb6fcca23264faf017b5fc880b7fd8d43a7ec28
anilmauryanet <anil.maurya.net@gmail.com>
2024-12-03 02:53:35 +0530

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DattingApp.Data;
+using DattingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public DataContext _Context { get; }
+        public UsersController(DataContext context)
+        {
+            _Context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            var users = await _Context.User.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUse(int id)
+        {
+            var user = await _Context.User.FindAsync(id);
+            return user;
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DattingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace DattingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> User { get; set; }
+    }
+}
